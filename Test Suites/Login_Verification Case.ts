8bd2075ce051e2bc935a6b8f5a5478fbce55e7e8
ShinKwangHo <hachi_mao@nate.com>
2018-02-19 14:29:41 +0900

    test message

--- Test Suites/Login_Verification Case.ts
+++ Test Suites/Login_Verification Case.ts
@@ -4,7 +4,7 @@
    <name>Login_Verification Case</name>
    <tag></tag>
    <isRerun>false</isRerun>
-   <lastRun>2018-02-19T14:19:32</lastRun>
+   <lastRun>2018-02-19T14:29:10</lastRun>
    <mailRecipient></mailRecipient>
    <numberOfRerun>0</numberOfRerun>
    <pageLoadTimeout>30</pageLoadTimeout>
